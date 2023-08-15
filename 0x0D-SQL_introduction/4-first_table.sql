87ed62f55a61e717066271478dc1b409512656fe
fredcheru <fredcheru10@gmail.com>
2023-08-15 16:53:25 -0700

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,1 +1,4 @@
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+-- MySQL create table in current database
+-- cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+    CREATE TABLE IF NOT EXISTS first_table (id INT,
+    name VARCHAR(256));
