3c31e0aa65f6ef4ec0cd1e23b62171bd29635d7a
fredcheru <fredcheru10@gmail.com>
2023-08-15 17:11:11 -0700

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- MySql script that converts database to UTF8
+-- cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p 
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
