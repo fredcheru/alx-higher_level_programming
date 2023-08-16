0748ec9d59628dee89bb78c4d087ff3749a96c10
fredcheru <fredcheru10@gmail.com>
2023-08-15 17:01:00 -0700

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,9 @@
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+-- MySQL script to create table with description
+-- cat 9-full_creation.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+name VARCHAR(256),
+score INT);
 
--- Script should create maximum of 4 records
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
