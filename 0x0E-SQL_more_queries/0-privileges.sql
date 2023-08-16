8b1c8a0565c0e7cc98a549a4b536127dd9c6eab3
fredcheru <fredcheru10@gmail.com>
2023-08-16 15:36:45 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- MySQL script to list all privileges
--- cat 0-privileges.sql | mysql -hlocalhost -uroot -p
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+#!/usr/bin/env bash
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS for user_0d_1@localhost;
+SHOW GRANTS for user_0d_2@localhost;
