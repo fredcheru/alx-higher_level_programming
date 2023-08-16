d429dc0c6c459c59fba503e685a9a443e96ccdd6
fredcheru <fredcheru10@gmail.com>
2023-08-15 17:09:06 -0700

    15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,6 @@
-SELECT score, COUNT(1) AS number FROM second_table
+-- lists number of records with hte same score
+-- cat 15-groups.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT `score`, COUNT(Id) AS "number"
+FROM second_table
 GROUP BY score
-ORDER BY number DESC;
+ORDER BY score DESC;
