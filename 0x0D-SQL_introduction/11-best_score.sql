030ac47f6aaf8394fd70926cbec5cc055ac7c768
fredcheru <fredcheru10@gmail.com>
2023-08-15 17:04:33 -0700

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,1 +1,6 @@
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+-- mySql script that lists scores >= 10
+-- cat 11-best_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT `score`,`name` 
+FROM second_table 
+WHERE score >= 10
+ORDER BY `score` DESC;
