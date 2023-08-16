0dc3eb820c14e25f226906305551ccc62f2e8785
fredcheru <fredcheru10@gmail.com>
2023-08-15 17:07:57 -0700

    14-average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,1 +1,3 @@
-SELECT AVG(score) AS average FROM second_table;
+-- MySQL script to calculate average of score
+-- cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT AVG(score) as `average` FROM second_table;
