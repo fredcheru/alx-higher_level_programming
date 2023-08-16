cd6bf83133e23ff3ae51e6f4101aaee4ff58a8c5
fredcheru <fredcheru10@gmail.com>
2023-08-15 17:03:16 -0700

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,3 @@
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- MySQL script to list score and name sort by score
+-- cat 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT `score`,`name` FROM second_table ORDER BY `score` DESC
