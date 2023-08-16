721f857ab94d3d8cb3aec8acc57c2d1d871406dc
fredcheru <fredcheru10@gmail.com>
2023-08-15 17:10:04 -0700

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,6 @@
-SELECT score, name
+-- list all records of the table
+-- cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT `score`, `name`
 FROM second_table
-HAVING name IS NOT NULL
+WHERE `name` IS NOT NULL
 ORDER BY score DESC;
