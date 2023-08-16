3ceeb64441d497d6861a6dd24df199deb33258ab
fredcheru <fredcheru10@gmail.com>
2023-08-15 17:05:42 -0700

    12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,1 +1,5 @@
-UPDATE second_table SET score = 10 WHERE name = "Bob";
+-- MySql script to update score
+-- cat 12-no_cheating.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+UPDATE second_table 
+SET score = 10
+WHERE `name` = "Bob";
