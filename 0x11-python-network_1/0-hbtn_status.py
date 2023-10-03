ac8df844d73fc7a05e8043e60f3b57e31b339bcf
fredcheru <fredcheru10@gmail.com>
2023-10-02 18:51:03 -0700

    fix 0,4

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-'''
-Script that fetches https://intranet.hbtn.io/status
-'''
-from urllib import request
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+"""
 
-url = 'https://intranet.hbtn.io/status'
-with request.urlopen(url) as response:
-    the_page = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(the_page)))
-    print('\t- content: {}'.format(the_page))
-    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        the_page = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf8 content: {}".format(the_page.decode('utf-8')))
