d9fb5617e535d7dac389d69f81142aeeda658705
fredcheru <fredcheru10@gmail.com>
2023-10-02 18:32:51 -0700

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-'''
-Script that fetches https://intranet.hbtn.io/status
-'''
+"""
+    Fetche https://intranet.hbtn.io/status
+"""
+
 import requests
 
 if __name__ == "__main__":
-    url = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(url.text)))
-    print('\t- content: {}'.format(url.text))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
